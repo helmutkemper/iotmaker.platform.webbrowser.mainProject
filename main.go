c81793b720a26a59ae6d4710e4645f52a16a25f5
Helmut Kemper <helmut.kemper@gmail.com>
2019-11-24 16:09:15 -0300

    trabalhando

--- main.go
+++ main.go
@@ -2,10 +2,22 @@
 
 package main
 
-import iotmaker_platform_webbrowser "github.com/helmutkemper/iotmaker.platform.webbrowser"
+import (
+	iotmaker_platform "github.com/helmutkemper/iotmaker.platform"
+	iotmaker_platform_webbrowser "github.com/helmutkemper/iotmaker.platform.webbrowser"
+	"github.com/helmutkemper/iotmaker.platform/abstractType"
+)
 
 func main() {
 
-	iotmaker_platform_webbrowser.NewStage("stage", 0, 0, 300, 300, 1)
+	_, stage := iotmaker_platform_webbrowser.NewStage("stage", 300, 300, 1)
+
+	var draw iotmaker_platform.ICanvas = &stage.Canvas
+	abstractType.NewBasicBox(draw)
+
+	stage.Canvas.BeginPath()
+	stage.Canvas.MoveTo(0, 0)
+	stage.Canvas.LineTo(300, 300)
+	stage.Canvas.Stroke()
 
 }
