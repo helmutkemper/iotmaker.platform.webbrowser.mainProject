461b4632550394c8d9a9fce05b7c3ceb7ebd4fd8
Helmut Kemper <helmut.kemper@gmail.com>
2019-11-25 19:44:30 -0300

    trabalhando

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ package main
 import (
 	iotmaker_platform_webbrowser "github.com/helmutkemper/iotmaker.platform.webbrowser"
 	"github.com/helmutkemper/iotmaker.platform/abstractType"
+	"image/color"
 )
 
 func main() {
@@ -17,15 +18,30 @@ func main() {
 	)
 
 	abstractType.NewBasicBox(
-		&stage.Canvas,
-		"box",
-		1.0,
-		10,
-		10,
-		200,
-		200,
-		50,
-		3,
+		abstractType.BasicBox{
+			Platform: &stage.Canvas,
+			Id:       "line",
+			Dimensions: abstractType.DimensionsBasicBox{
+				X:         20,
+				Y:         50,
+				Width:     100,
+				Height:    100,
+				Border:    5,
+				LineWidth: 2,
+				Density:   1.0,
+			},
+			Shadow: abstractType.Shadow{
+				Color: color.RGBA{
+					R: 0,
+					G: 0,
+					B: 0,
+					A: 50,
+				},
+				Blur:    3,
+				OffsetX: 1,
+				OffsetY: 1,
+			},
+		},
 	)
 
 	abstractType.NewLineTo(
