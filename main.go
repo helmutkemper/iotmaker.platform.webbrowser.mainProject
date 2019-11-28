2a745a11b6cd1a593fbf901f87195852ff7133e2
Helmut Kemper <helmut.kemper@gmail.com>
2019-11-28 20:57:31 -0300

    trabalhando

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ package main
 import (
 	iotmaker_platform_webbrowser "github.com/helmutkemper/iotmaker.platform.webbrowser"
 	"github.com/helmutkemper/iotmaker.platform/abstractType"
+	"github.com/helmutkemper/iotmaker.platform/abstractType/colornames"
 	"github.com/helmutkemper/iotmaker.platform/abstractType/gradient"
 	"github.com/helmutkemper/iotmaker.platform/abstractType/shadow"
 	"image/color"
@@ -21,8 +22,8 @@ func main() {
 
 	shadowBox := shadow.NewShadow(color.RGBA{R: 120, G: 0, B: 0, A: 255}, 10, 4, 4)
 	coordinateBox := gradient.NewCoordinate(0, 0, 120, 150)
-	colorWhite := gradient.NewColorPosition(color.RGBA{R: 0xFF, G: 0xFF, B: 0xFF, A: 0x00}, 0.2)
-	colorBlack := gradient.NewColorPosition(color.RGBA{R: 0x00, G: 0x00, B: 0x00, A: 0xFF}, 1)
+	colorWhite := gradient.NewColorPosition(colornames.WhiteTransparent, 0.2)
+	colorBlack := gradient.NewColorPosition(colornames.Black, 1)
 	colorList := gradient.NewColorList(colorBlack, colorWhite)
 	gradientBox := gradient.NewStrokeGradientLinear(coordinateBox, colorList)
 
