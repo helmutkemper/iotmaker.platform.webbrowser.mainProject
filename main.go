de3f32d4585d868c6d7db51025cbd8bca3ab11ca
Helmut Kemper <helmut.kemper@gmail.com>
2019-12-03 19:55:06 -0300

    trabalhando

--- main.go
+++ main.go
@@ -3,7 +3,7 @@
 package main
 
 import (
-	"fmt"
+	iotmaker_platform_IDraw "github.com/helmutkemper/iotmaker.platform.IDraw"
 	coordinateManager "github.com/helmutkemper/iotmaker.platform.coordinate"
 	"github.com/helmutkemper/iotmaker.platform.webbrowser/canvas"
 	"github.com/helmutkemper/iotmaker.platform.webbrowser/document"
@@ -11,18 +11,32 @@ import (
 	"github.com/helmutkemper/iotmaker.platform/abstractType/basicBox"
 	"github.com/helmutkemper/iotmaker.platform/abstractType/colornames"
 	"github.com/helmutkemper/iotmaker.platform/abstractType/gradient"
+	"github.com/helmutkemper/iotmaker.platform/abstractType/shadow"
+	"image/color"
+)
+
+var (
+	colorShadow    color.RGBA
+	filter         iotmaker_platform_IDraw.IFilterShadowInterface
+	density                                   = 3.0
+	densityManager coordinateManager.IDensity = &coordinateManager.Density{}
+	bx2                                       = &basicBox.BasicBox{}
+	stage                                     = canvas.Stage{}
+	gradientFilter iotmaker_platform_IDraw.IFilterGradientInterface
 )
 
 func main() {
 
 	done := make(chan struct{}, 0)
 
-	var density = 3.0
-	var densityManager coordinateManager.IDensity = &coordinateManager.Density{}
-
 	browserDocument := document.NewDocument()
 
-	stage := canvas.NewStage(
+	colorShadow = colornames.DarkblueTransparent
+	filter = shadow.NewShadowFilter(colorShadow, 5, 2, 2, density, densityManager)
+
+	//mouse.AddFunctionPointer(bx1.GetAlphaChannel)
+
+	stage = canvas.NewStage(
 		browserDocument,
 		"stage",
 		300,
@@ -31,19 +45,16 @@ func main() {
 		densityManager,
 	)
 
-	//colorDarkBlue := colornames.Darkblue
-	//shadowFilter := shadow.NewShadowFilter(colorDarkBlue, 5, 2, 2, density, densityManager)
-
 	colorWhite := gradient.NewColorPosition(colornames.WhiteTransparent, 0.5)
 	colorBlack := gradient.NewColorPosition(colornames.Black, 1)
 	colorList := gradient.NewColorList(colorBlack, colorWhite)
 
 	coordinateP0 := gradient.NewPoint(0, 0, density, densityManager)
 	coordinateP1 := gradient.NewPoint(120, 150, density, densityManager)
-	gradientFilter := gradient.NewGradientLinearToStroke(coordinateP0, coordinateP1, colorList)
+	gradientFilter = gradient.NewGradientLinearToStroke(coordinateP0, coordinateP1, colorList)
 
 	basicBox.NewBasicBox(&stage.Canvas, &stage.ScratchPad, "bbox_1", 20, 50, 100, 100, 5, 5, nil, nil, density, densityManager)
-	bx2 := basicBox.NewBasicBox(
+	bx2 = basicBox.NewBasicBox(
 		&stage.Canvas,
 		&stage.ScratchPad,
 		"bbox_2",
@@ -53,7 +64,7 @@ func main() {
 		100,
 		10,
 		8,
-		nil, //shadowFilter,
+		filter,
 		gradientFilter,
 		density,
 		densityManager,
@@ -72,13 +83,30 @@ func main() {
 		1,
 	)*/
 
-	//mouse.AddFunctionPointer(bx1.GetAlphaChannel)
-	mouse.AddFunctionPointer(bx2.GetAlphaChannel, bateu)
 	browserDocument.SetMouseMoveListener(mouse.GetDefaultFunction())
+	mouse.AddFunctionPointer(bx2.GetAlphaChannel, bateu)
 
 	<-done
 }
 
 func bateu(x, y int) {
-	fmt.Printf("bateu nas coordenadas x:%v, y:%v\n", x, y)
+
+	colorShadow = colornames.Darkred
+	filter = shadow.NewShadowFilter(colorShadow, 5, 2, 2, density, densityManager)
+
+	bx2 = basicBox.NewBasicBox(
+		&stage.Canvas,
+		&stage.ScratchPad,
+		"bbox_2",
+		20+50,
+		50+50,
+		100,
+		100,
+		10,
+		8,
+		filter,
+		gradientFilter,
+		density,
+		densityManager,
+	)
 }
