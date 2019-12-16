4d62a49a174a7d45f57eaaaaaa5c4d7833e5e9d0
Helmut Kemper <helmut.kemper@gmail.com>
2019-12-16 20:37:34 -0300

    tween graph

--- example_newTweenLinear/main.go
+++ example_newTweenLinear/main.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"fmt"
 	iotmakerPlatformIDraw "github.com/helmutkemper/iotmaker.platform.IDraw"
 	coordinateManager "github.com/helmutkemper/iotmaker.platform.coordinate"
 	"github.com/helmutkemper/iotmaker.platform.webbrowser/Html"
@@ -12,6 +13,7 @@ import (
 	"github.com/helmutkemper/iotmaker.platform.webbrowser/factoryBrowserStage"
 	"github.com/helmutkemper/iotmaker.platform/factoryDraw"
 	"github.com/helmutkemper/iotmaker.platform/factoryTween"
+	"github.com/helmutkemper/iotmaker.platform/fps"
 	"time"
 )
 
@@ -33,17 +35,18 @@ func main() {
 	//imgHtml := factoryImage.NewHtmlImage(html, browserDocument.SelfDocument, map[string]interface{}{"id":  "player", "src": "./player_big.png"},true,true)
 	//factoryImage.NewMultipleSpritesImage(&stage.Canvas, imgHtml,48,60,0,7, 90*time.Millisecond,50,70,48,60, density, densityManager).Crete()
 
-	cl := factoryDraw.NewChartLinear(&stage.Canvas, 10, 10, 200, 100, density, densityManager)
-	cl.Begin(10, 110)
-	initial := 10
-	step := 100 / 200
-	factoryTween.NewTweenEaseInQuadratic(
-		time.Second*10,
-		210,
+	cl := factoryDraw.NewChartLinear(&stage.Canvas, 10, 10, 600, 500, density, densityManager)
+	cl.Begin(10, 510)
+	x := 10.0
+	step := 590.0 / float64(fps.Get()) / 2.0
+	factoryTween.NewLinear(
+		time.Second*2,
 		10,
-		func(value float64) {
-			cl.Point(int(value), initial)
-			initial += step
+		500,
+		func(y float64) {
+			fmt.Printf("y: %v\n", y)
+			cl.Point(int(x), int(y))
+			x += step
 		},
 		func(value float64) {
 			cl.End()
