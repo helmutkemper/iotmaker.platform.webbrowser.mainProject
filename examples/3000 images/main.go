89351ac721e0cd0f53cc63661cd3a35e274d5bc3
Helmut Kemper <helmut.kemper@gmail.com>
2019-12-30 19:36:51 -0300

    trabalhando

--- examples/3000 images/main.go
+++ examples/3000 images/main.go
@@ -9,28 +9,24 @@ import (
 	"github.com/helmutkemper/iotmaker.platform.webbrowser/Html"
 	"github.com/helmutkemper/iotmaker.platform.webbrowser/canvas"
 	"github.com/helmutkemper/iotmaker.platform.webbrowser/document"
-	"github.com/helmutkemper/iotmaker.platform.webbrowser/eventMouse"
 	"github.com/helmutkemper/iotmaker.platform.webbrowser/factoryBrowserDocument"
 	"github.com/helmutkemper/iotmaker.platform.webbrowser/factoryBrowserImage"
 	"github.com/helmutkemper/iotmaker.platform.webbrowser/factoryBrowserStage"
-	webBrowserMouse "github.com/helmutkemper/iotmaker.platform.webbrowser/mouse"
-	"github.com/helmutkemper/iotmaker.platform/abstractType/draw"
 	"github.com/helmutkemper/iotmaker.platform/factoryImage"
-	"github.com/helmutkemper/iotmaker.platform/mouse"
+	"github.com/helmutkemper/iotmaker.platform/factoryTween"
+	"github.com/helmutkemper/iotmaker.platform/mathUtil"
+	"time"
 )
 
 var (
 	density                                   = 1.0
 	densityManager coordinateManager.IDensity = &coordinateManager.Density{}
-	bx2                                       = &draw.BasicBox{}
 	stage                                     = canvas.Stage{}
-	gradientFilter iotmakerPlatformIDraw.IFilterGradientInterface
 )
 
 var html iotmakerPlatformIDraw.IHtml
 var browserDocument document.Document
 var imgSpace interface{}
-var imgPlayer interface{}
 
 func prepareDataBeforeRun() {
 
@@ -55,17 +51,6 @@ func prepareDataBeforeRun() {
 		true,
 		false,
 	)
-
-	imgPlayer = factoryBrowserImage.NewImage(
-		html,
-		browserDocument.SelfDocument,
-		map[string]interface{}{
-			"id":  "player",
-			"src": "./player_big.png",
-		},
-		true,
-		false,
-	)
 }
 
 func main() {
@@ -73,22 +58,49 @@ func main() {
 	done := make(chan struct{}, 0)
 	prepareDataBeforeRun()
 
-	i := factoryImage.NewImage(
-		&stage.Canvas,
-		&stage.ScratchPad,
-		imgSpace,
-		-100,
-		-100,
-		29,
-		50,
-		density,
-		densityManager,
-	)
-	i.SetDraggable(true)
-	stage.Add(i.Draw)
-
-	browserDocument.AddEventListener(eventMouse.KMouseMove, webBrowserMouse.SetMouseMoveManager(mouse.ManagerMouseMove))
-	//mouse.AddFunctionPointer("bBox2", bx2.GetCollisionBox, bateu)
+	for a := 0; a != 3000; a += 1 {
+		i := factoryImage.NewImage(
+			&stage.Canvas,
+			&stage.ScratchPad,
+			imgSpace,
+			-100,
+			-100,
+			29,
+			50,
+			density,
+			densityManager,
+		)
+		//i.SetDraggable(true)
+		stage.Add(i.Draw)
+		factoryTween.NewSelectRandom(
+			time.Millisecond*time.Duration(mathUtil.Float64FomInt(500, 3000)),
+			mathUtil.Float64FomInt(0, 800),
+			mathUtil.Float64FomInt(0, 800),
+			nil,
+			nil,
+			nil,
+			func(x, p float64, ars ...interface{}) {
+				i.Dimensions.X = x
+				i.OutBoxDimensions.X = x
+			},
+			0,
+			nil,
+		)
+		factoryTween.NewSelectRandom(
+			time.Millisecond*time.Duration(mathUtil.Float64FomInt(500, 3000)),
+			mathUtil.Float64FomInt(0, 600),
+			mathUtil.Float64FomInt(0, 600),
+			nil,
+			nil,
+			nil,
+			func(y, p float64, ars ...interface{}) {
+				i.Dimensions.Y = y
+				i.OutBoxDimensions.Y = y
+			},
+			0,
+			nil,
+		)
+	}
 
 	<-done
 }
