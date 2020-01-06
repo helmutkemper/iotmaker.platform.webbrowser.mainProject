f4eab92add1e2aaa18525f798a9b6fa59f90923e
Helmut Kemper <helmut.kemper@gmail.com>
2020-01-05 22:41:53 -0300

    trabalhando

--- cmd/master/master.go
+++ cmd/master/master.go
@@ -9,8 +9,12 @@ import (
 	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/Html"
 	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/canvas"
 	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/document"
+	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/eventMouse"
 	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/factoryBrowserDocument"
+	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/factoryBrowserImage"
 	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/factoryBrowserStage"
+	webBrowserMouse "github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/mouse"
+	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform/factoryImage"
 	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform/mouse"
 )
 
@@ -22,6 +26,7 @@ var (
 
 var htmlElement iotmakerPlatformIDraw.IHtml
 var browserDocument document.Document
+var imgSpace Html.Image
 
 func prepareBeforeRun() {
 	htmlElement = &Html.Html{}
@@ -35,13 +40,37 @@ func prepareBeforeRun() {
 	)
 	stage.SetCursor(mouse.KCursorDefault)
 
-	stage.SetFps(60)
+	imgSpace = factoryBrowserImage.NewImage(
+		htmlElement,
+		browserDocument.SelfDocument,
+		map[string]interface{}{
+			"id":  "spacecraft",
+			"src": "./spacecraft.png",
+		},
+		true,
+		false,
+	)
 }
 
 func main() {
 	done := make(chan struct{}, 0)
 	prepareBeforeRun()
 
+	i := factoryImage.NewImage(
+		&stage.Canvas,
+		&stage.ScratchPad,
+		imgSpace.Get(),
+		10,
+		10,
+		88,
+		150,
+		density,
+		densityManager,
+	)
+	i.DragStart()
+	stage.AddToStage(i.Draw)
+
+	browserDocument.AddEventListener(eventMouse.KMouseMove, webBrowserMouse.SetMouseMoveManager(mouse.ManagerMouseMove))
 	//mouse.AddFunctionPointer("bBox2", bx2.GetCollisionBox, bateu)
 
 	<-done
